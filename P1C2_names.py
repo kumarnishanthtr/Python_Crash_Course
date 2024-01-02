eb773f7015a77bea8045474cc06abaa7762bfe1d
Nishanth Kumar <kumarnishanthtr@gmail.com>
2024-01-02 22:43:56 +0530

    Part 1 Chapter 2

---
+++ P1C2_names.py
@@ -0,0 +1,9 @@
+# `title()` displays each word in titlecase, where each word begins with a capital letter.
+
+name = "danny TaNNer"
+print(name.title())
+
+# other methods dealing with case
+print(name.upper())
+print(name.lower())
+
