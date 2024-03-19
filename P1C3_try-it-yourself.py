8e23c2ad6237d6d0d563387bfb4e2903a728830b
Nishanth Kumar <kumarnishanthtr@gmail.com>
2024-03-19 15:42:17 +0530

    updated

--- P1C3_try-it-yourself.py
+++ P1C3_try-it-yourself.py
@@ -84,4 +84,75 @@ not_asia = 'finland'
 asian_countries.remove(not_asia)
 print(not_asia.title() + ' is not in Asia!')
 
+# 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner . Then use your list to print a message to each person, inviting
+# them to dinner .
+invite_list = ['Poorna Chandra Tejaswi', 'Shivarama Karanth', 'Arsene Wenger', 'J.R.R Tolkien']
+print('Respected ' + invite_list[0] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[1] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[2] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[3] + ', this is an invitation for the dinner.')
 
+# 3-5. Changing Guest List: You just heard that one of your guests can’t make the
+# dinner, so you need to send out a new set of invitations . You’ll have to think of
+# someone else to invite 
+invite_list = ['Poorna Chandra Tejaswi', 'Shivarama Karanth', 'Arsene Wenger', 'J.R.R Tolkien']
+print('Sorry to have been known that Mr.'+invite_list[-2]+ ' will not be attending the dinner.')
+invite_list[-2] = 'Narendra Modi'
+print('Respected ' + invite_list[0] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[1] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[2] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[3] + ', this is an invitation for the dinner.')
+
+
+# 3-6. More Guests: You just found a bigger dinner table, so now more space is
+# available . Think of three more guests to invite to dinner 
+invite_list = ['Poorna Chandra Tejaswi', 'Shivarama Karanth', 'Narendra Modi', 'J.R.R Tolkien']
+invite_list.insert(0, 'Rukmini Vasanth')
+invite_list.insert(3, 'Swathi HC')
+invite_list.append('Mammootty')
+
+print('Respected ' + invite_list[0] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[1] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[2] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[3] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[4] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[5] + ', this is an invitation for the dinner.')
+print('Respected ' + invite_list[6] + ', this is an invitation for the dinner.')
+
+# 3-7. Shrinking Guest List: You just found out that your new dinner table won’t
+# arrive in time for the dinner, and you have space for only two guests 
+
+invite_list = ['Rukmini Vasanth',
+ 'Poorna Chandra Tejaswi',
+ 'Shivarama Karanth',
+ 'Swathi HC',
+ 'Narendra Modi',
+ 'J.R.R Tolkien',
+ 'Mammootty']
+
+print('Unfortunately, we can only have two guests for the dinner :(')
+
+removed_guest = invite_list.pop()
+print('Respected '+ removed_guest.title() + ', Sorry for inconvenience caused.')
+
+removed_guest = invite_list.pop()
+print('Respected '+ removed_guest.title() + ', Sorry for inconvenience caused.')
+
+removed_guest = invite_list.pop()
+print('Respected '+ removed_guest.title() + ', Sorry for inconvenience caused.')
+
+removed_guest = invite_list.pop(0)
+print('Respected '+ removed_guest.title() + ', Sorry for inconvenience caused.')
+
+removed_guest = invite_list.pop(1)
+print('Respected '+ removed_guest.title() + ', Sorry for inconvenience caused.')
+
+print('Dear '+ invite_list[0] + ', Happy to inform you that you are still invited.')
+print('Dear '+ invite_list[1] + ', Happy to inform you that you are still invited.')
+
+del invite_list[0]
+del invite_list[0]
+
+print(invite_list)
