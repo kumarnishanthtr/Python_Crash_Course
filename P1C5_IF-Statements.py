948a88798877f65e9386ef5e48f2e36b53baf83d
Nishanth Kumar <kumarnishanthtr@gmail.com>
2024-07-22 16:41:22 +0530

    updated

--- P1C5_IF-Statements.py
+++ P1C5_IF-Statements.py
@@ -1,106 +1,127 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 20 12:31:57 2024
-
-@author: nikumar
-"""
-# IF Condition
-bikes = ['tvs', 'royal Enfield', 'Ola', 'hero', 'Ather', 'ktm']
-
-for bike in bikes:
-    if bike == 'tvs' or bike == 'ktm':
-        print(bike.upper())
-    else:
-        print(bike.title())
-        
-
-age = 18
-age >= 18
-age < 18
-age == 18
-age <= 18
-
-age_me = 38
-age_friend = 32
-
-if age_me < 35 and age_friend < 35 :
-    print('both are less than 35')
-else:
-    print('someone is more than 35')
-
-
-# Checking Whether a Value Is in a List
-'Ola' in bikes
-'Bajaj' in bikes
-
-# Checking Whether a Value Is Not in a List
-unsafe_countries = ['Pakistan', 'Bangladesh', 'Russia', 'Syria', 'Iraq']
-
-user_wish = 'India'
-
-if user_wish not in unsafe_countries:
-    print('You can travel to ', user_wish.title(), '. It is safe! ')
-else:
-    print('Please do not go to', user_wish.title())
-    
-    
-# if-else Statements
-print("Enter your age!")
-age = int(input())
-
-if age >= 18:
-    print("You are old enough to vote!")
-    print("Have you registered to vote yet?")
-else:
-    print("Sorry. You are not old enough to vote!")
-    print("Please register to vote as soon as you turn 18")
-print("Thank you for using this service!")
-
-
-# if - elif - else
-# consider an amusement park that charges different rates for different age groups:
-# Admission for anyone under age 4 is free.
-# Admission for anyone between the ages of 4 and 18 is $5.
-# Admission for anyone age 18 or older is $10.
-
-# method 1
-
-print("Welcome to the NS Amusement Park!. Please enter your age")
-age = int(input())
-if age <= 4:
-    print("Your admission fee is 0$")
-elif age >= 5 and age <= 17:
-    print("Your admission fee is 5$")
-else:
-    print("Your admission fee is 10$")
-
-
-# method 2
-
-print("Welcome to the NS Amusement Park!. Please enter your age")
-age = int(input())
-price = 0
-if age <= 4:
-    price = 0
-elif age <= 18:
-    price = 5
-elif age > 65:
-    price = 5
-else:
-    price = 5
-
-print("Your admission fee is " + str(price) + "$")
-
-# second method is better since it avoids repetitive messages. 
-# to change the output statement, we can change once compared to thrice in method 1
-
-
+# # -*- coding: utf-8 -*-
+# """
+# Created on Thu Jun 20 12:31:57 2024
+# 
+# @author: nikumar
+# """
+# # IF Condition
+# bikes = ['tvs', 'royal Enfield', 'Ola', 'hero', 'Ather', 'ktm']
+# 
+# for bike in bikes:
+#     if bike == 'tvs' or bike == 'ktm':
+#         print(bike.upper())
+#     else:
+#         print(bike.title())
+#         
+# 
+# age = 18
+# age >= 18
+# age < 18
+# age == 18
+# age <= 18
+# 
+# age_me = 38
+# age_friend = 32
+# 
+# if age_me < 35 and age_friend < 35 :
+#     print('both are less than 35')
+# else:
+#     print('someone is more than 35')
+# 
+# 
+# # Checking Whether a Value Is in a List
+# 'Ola' in bikes
+# 'Bajaj' in bikes
+# 
+# # Checking Whether a Value Is Not in a List
+# unsafe_countries = ['Pakistan', 'Bangladesh', 'Russia', 'Syria', 'Iraq']
+# 
+# user_wish = 'India'
+# 
+# if user_wish not in unsafe_countries:
+#     print('You can travel to ', user_wish.title(), '. It is safe! ')
+# else:
+#     print('Please do not go to', user_wish.title())
+#     
+#     
+# # if-else Statements
+# print("Enter your age!")
+# age = int(input())
+# 
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry. You are not old enough to vote!")
+#     print("Please register to vote as soon as you turn 18")
+# print("Thank you for using this service!")
+# 
+# 
+# # if - elif - else
+# # consider an amusement park that charges different rates for different age groups:
+# # Admission for anyone under age 4 is free.
+# # Admission for anyone between the ages of 4 and 18 is $5.
+# # Admission for anyone age 18 or older is $10.
+# 
+# # method 1
+# 
+# print("Welcome to the NS Amusement Park!. Please enter your age")
+# age = int(input())
+# if age <= 4:
+#     print("Your admission fee is 0$")
+# elif age >= 5 and age <= 17:
+#     print("Your admission fee is 5$")
+# else:
+#     print("Your admission fee is 10$")
+# 
+# 
+# # method 2
+# 
+# print("Welcome to the NS Amusement Park!. Please enter your age")
+# age = int(input())
+# price = 0
+# if age <= 4:
+#     price = 0
+# elif age <= 18:
+#     price = 5
+# elif age > 65:
+#     price = 5
+# else:
+#     price = 5
+# 
+# print("Your admission fee is " + str(price) + "$")
+# 
+# # second method is better since it avoids repetitive messages. 
+# # to change the output statement, we can change once compared to thrice in method 1
+# 
+# 
 # using if statements with lists
+# 
+# requested_flavours = ['vanilla', 'lychee', 'blueberry', 'pista', 'banana']
+# 
+# for topping in requested_flavours:
+#     if topping == 'lychee':
+#         print("sorry we are not having lychee flavour!")
+#     else:
+#         print("finished adding " + topping)
 
-requested_flavours = ['vanilla', 'lychee', 'blueberry']
+# Checking That a List Is Not Empty
+# requested_flavours = []
+# if requested_flavours:
+#     for requested_flavour in requested_flavours:
+#         print("finished adding " + requested_flavour + " flavour")
+# else:
+#     print("you have not selected any flavour. do you want a plain juice?")
 
-for topping in requested_flavours:
-    if topping == 'lychee':
-        print("sorry we are not having lychee flavour!")
+# using multiple lists
+available_flavours = ['mint', 'cola', 'lime', 'dry fruits', 'banana', 'chocolate']
+
+requested_flavours = ['lime', 'cola', 'mango', 'chocolate']
+print("Customer requested flavours: " + str(requested_flavours))
+for requested_flavour in requested_flavours:
+    if requested_flavour in available_flavours:
+        print("Finished adding " + requested_flavour + ".")
     else:
-        print("finished adding " + topping)
+        print("Sorry. We are out of "+ requested_flavour + "!")
+
+
--- P1C5_try-it-yourself.py
+++ P1C5_try-it-yourself.py
@@ -166,3 +166,84 @@ if 'blueberry' in favourite_fruits:
 
 if 'papaya' in favourite_fruits:
     print("you really like papaya")
+
+
+# 5.8 Hello Admin: Make a list of five or more usernames, including the name
+# 'admin' . Imagine you are writing code that will print a greeting to each user
+# after they log in to a website . Loop through the list, and print a greeting to
+# each user:
+
+# If the username is 'admin', print a special greeting, such as Hello admin,
+#would you like to see a status report?
+
+# Otherwise, print a generic greeting, such as Hello Eric, thank you for log-
+# ging in again.
+
+#user_list = ['furiousfrodo', 'admin', 'sweet1214', 'admin', 'nimittha']
+
+for user in user_list:
+    if user == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello "+ user + "!, Thank you for logging in!")
+        
+
+# 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is
+# not empty .
+# If the list is empty, print the message We need to find some users!
+# Remove all of the usernames from your list, and make sure the correct
+# message is printed
+user_list = []
+
+if user_list:
+    if user == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello "+ user + "!, Thank you for logging in!")
+else:
+    print("We need to find some users!")
+
+
+# 5-10. Checking Usernames: Do the following to create a program that simulates
+# how websites ensure that everyone has a unique username .
+# Make a list of five or more usernames called current_users .
+# Make another list of five usernames called new_users . Make sure one or
+# two of the new usernames are also in the current_users list .
+# Loop through the new_users list to see if each new username has already
+# been used . If it has, print a message that the person will need to enter a
+# new username . If a username has not been used, print a message saying
+# that the username is available .
+# Make sure your comparison is case insensitive . If 'John' has been used,
+# 'JOHN' should not be accepted .
+
+current_users = ['john', 'spex', 'zuck', 'nish', 'swa']
+new_users = ['nimi', 'NISH', 'swathi', 'max', 'John']
+[x.lower() for x in current_users] # list comprehension
+for user in new_users:
+    
+    if user.lower() in current_users:
+        print (user + ": Sorry that username is taken")
+    else:
+        print(user + ": Username is available!")
+        
+
+# 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such
+# as 1st or 2nd . Most ordinal numbers end in th, except 1, 2, and 3 .
+# Store the numbers 1 through 9 in a list .
+# Loop through the list .
+# Use an if-elif-else chain inside the loop to print the proper ordinal end-
+# ing for each number . Your output should read "1st 2nd 3rd 4th 5th 6th
+# 7th 8th 9th", and each result should be on a separate line .
+
+numbers = list(range(1,10))
+
+for number in numbers:
+    if number == 1:
+        print('1st \n')
+    elif number == 2:
+        print('2nd \n')
+    elif number == 3:
+        print('3rd \n')
+    else:
+        print(str(number) + 'th \n')
+        
